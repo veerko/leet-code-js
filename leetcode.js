ed0e17efd60f8c3357641d78e189d5ddfb993d11
st1ko <74153758+st1ko-gs@users.noreply.github.com>
2023-03-10 10:22:02 +0200

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -17,3 +17,19 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 **/
+
+
+var zigzagLevelOrder = function(root) {
+    let results = [];
+    const lot = (root, level) => {
+        if(!root) return;
+        
+        if(results[level]) results[level].push(root.val);
+        else results[level] = [root.val];
+        
+        lot(root.left, level+1);
+        lot(root.right, level+1);
+    }
+    lot(root, 0);
+    return results.map((b,i)=>(i%2) ? b.reverse(): b);
+};
